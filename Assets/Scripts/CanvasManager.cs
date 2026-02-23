1b37f8de3f0ff42ed96b6986d071fb1fcdd20b63
cfrogggyrena <esguerca@sheridancollege.ca>
2026-02-22 22:58:24 -0500

    created terrain + added player tag

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class CanvasManager : MonoBehaviour
 {
 
+    [SerializeField]
+    private TMP_InputField usernameInput;
 
 
     //START AND FINISH GAME FOR HOME SCENE:
     public void startGameButton()
     {
-        //loads scene with index 1 when function is used
+        //INPUT SETTINGS FOR USERNAME
+        string name = usernameInput.text?.Trim();
+
+        if (string.IsNullOrEmpty(name))
+        {
+            // show small warning in case
+            UnityEngine.Debug.Log("Please enter a username.");
+            return;
+        }
+
+        // make sure there's a session holder (added on usersession)
+        if (UserSession.Instance == null)
+        {
+            //self check in case i forget tod add usersession lol
+            UnityEngine.Debug.LogError("UserSession is missing in this scene. Add the UserSession prefab/object on hierarchy pls :D.");
+            return;
+        }
+
+        UserSession.Instance.SetUsername(name);
+
+        //LOADS SCENE WITH INDEX NUMBER 1
         SceneManager.LoadScene(1);
     }
 
+    //EXIT GAME BUTTON
     public void exitGameButton()
     {
         //tried to do application.quit but it wasn't working on engine so I found this solution online :(
@@ -28,7 +52,9 @@ public class CanvasManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // If the user navigated back to this scene, repopulate if needed
+        if (UserSession.Instance != null && !string.IsNullOrEmpty(UserSession.Instance.Username))
+            usernameInput.text = UserSession.Instance.Username;
     }
 
     // Update is called once per frame
